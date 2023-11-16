9b28096a9cb69ddcb3e576af2f468d3a9d69a367
Alex <awatt012@yahoo.com>
2023-11-15 19:08:04 -0700

    source bubble

--- dist/components/bubbles/SourceBubble.d.ts
+++ dist/components/bubbles/SourceBubble.d.ts
@@ -4,5 +4,10 @@ type Props = {
     onSourceClick?: () => void;
 };
 export declare const SourceBubble: (props: Props) => import("solid-js").JSX.Element;
-export {};
+type BubbleArrayProps = {
+    bubbles: Props[];
+};
+export declare const SourceBubblesContainer: ({ bubbles }: BubbleArrayProps) => import("solid-js").JSX.Element;
+declare const App: () => import("solid-js").JSX.Element;
+export default App;
 //# sourceMappingURL=SourceBubble.d.ts.map
