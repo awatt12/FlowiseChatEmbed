cf226c6abad0a20674e822bb668e887740d9f1d5
Alex <awatt012@yahoo.com>
2023-11-15 19:22:57 -0700

    source bubble

--- dist/components/bubbles/SourceBubble.d.ts
+++ dist/components/bubbles/SourceBubble.d.ts
@@ -4,10 +4,5 @@ type Props = {
     onSourceClick?: () => void;
 };
 export declare const SourceBubble: (props: Props) => import("solid-js").JSX.Element;
-type BubbleArrayProps = {
-    bubbles: Props[];
-};
-export declare const SourceBubblesContainer: ({ bubbles }: BubbleArrayProps) => import("solid-js").JSX.Element;
-declare const App: () => import("solid-js").JSX.Element;
-export default App;
+export {};
 //# sourceMappingURL=SourceBubble.d.ts.map
--- src/components/bubbles/SourceBubble.tsx
+++ src/components/bubbles/SourceBubble.tsx
@@ -1,65 +1,22 @@
-// Step 1: Define the SourceBubble component
 type Props = {
-  pageContent: string;
-  metadata: object;
-  onSourceClick?: () => void;
-};
-
+  pageContent: string
+  metadata: object
+  onSourceClick?: () => void
+}
 export const SourceBubble = (props: Props) => (
-  <div
-    data-modal-target="defaultModal" 
-    data-modal-toggle="defaultModal"
-    className="flex justify-start mb-2 items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
-    onClick={() => props.onSourceClick?.()}
-  >
-    <span
-      className="px-2 py-1 ml-1 whitespace-pre-wrap chatbot-host-bubble"
-      data-testid="host-bubble"
-      style={{
-        "min-width": '80px',
-        "max-width": '80%',
-        "font-size": "13px",
-        "border-radius": '15px',
-        cursor: 'pointer',
-        "background": "#E1EFFF",
-        "word-wrap": "break-word",
-        "overflow": "auto",
-      }}
+  <>
+    <div
+      data-modal-target="defaultModal" data-modal-toggle="defaultModal"
+      class="flex justify-start mb-2 items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
+      onClick={() => props.onSourceClick?.()}
     >
-      {props.pageContent}
-    </span>
-  </div>
-);
-
-// Step 2: Define the SourceBubblesContainer component
-type BubbleArrayProps = {
-  bubbles: Props[];
-};
-
-export const SourceBubblesContainer = ({ bubbles }: BubbleArrayProps) => (
-  <div className="flex flex-col justify-start items-start">
-    {bubbles.map((bubble, index) => (
-      <SourceBubble key={index} {...bubble} />
-    ))}
-  </div>
-);
-
-// Step 3: Create an array of data for the bubbles
-const bubblesData: Props[] = [
-  // ... Populate with your actual data
-];
-
-// Step 4: Use the SourceBubblesContainer in your app's render method
-const App = () => {
-  // ... your other component logic
-
-  return (
-    <div>
-      {/* ... other parts of your application */}
-      <SourceBubblesContainer bubbles={bubblesData} />
-      {/* ... other parts of your application */}
+      <span
+        class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
+        data-testid="host-bubble"
+        style={{ width: 'max-content', "max-width": '80px', "font-size": "13px", "border-radius": '15px', cursor: 'pointer', "text-overflow": "ellipsis", "overflow": "hidden", "white-space": "nowrap" }}
+      >
+        {props.pageContent}
+      </span>
     </div>
-  );
-};
-
-export default App;
+  </>
+)
